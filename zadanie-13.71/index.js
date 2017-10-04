57d9a0e5fa310c237fb4933c2a572e71411589c3
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2017-10-04 06:53:40 +0200

    second commit

--- zadanie-13.71/index.js
+++ zadanie-13.71/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var colors = require('colors');
 
-fs.readdir('../zadanie-13.7', 'utf-8', function(err, files) {
+fs.readdir('../zadanie-13.71', 'utf-8', function(err, files) {
 	console.log(files);
 	fs.writeFile('./plik.txt', files ,  function(err) {
 		if (err) throw err;
